0e3e84156dae06aa101484cf57f53cca42628e0f
Karthik Seenuvasan <yeskay16@gmail.com>
2025-09-06 11:26:54 +0200

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,6 +2,7 @@ def call(String runScript, String outputFolder) {
     if (fileExists("${outputFolder}")) {
         echo '🚀 Deploying the app...'
         sh "${runScript}"
+        sh "sleep 25"
     } else {
         echo "⚠️ Skipping deployment - output folder '${outputFolder}' not found."
     }
